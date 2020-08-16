745c210cb49fb881700dabecb6ea0bc75aa42743
Nancy4510 <nancy4510@gmail.com>
2020-08-15 22:46:02 -0400

    Changed name of the environment variable from MONGODB_URI to DB_URI
    The mLab MongoDB add-on will be discontinued and removed from Heroku
    in November, so I had to migrate a sandbox Heroku add-on to
    MongoDB Atlas.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'brooklyn-pet-api'
 
 // create the mongodb uri for development and test
 const database = {
@@ -13,8 +13,8 @@ const database = {
 // select DB based on whether a test file was executed before `server.js`
 const localDb = process.env.TESTENV ? database.test : database.development
 
-// Environment variable MONGODB_URI will be available in
+// Environment variable MONGODB_URI (now known as DB_URI) will be available in
 // heroku production evironment otherwise use test or development db
-const currentDb = process.env.MONGODB_URI || localDb
+const currentDb = process.env.DB_URI || localDb
 
 module.exports = currentDb
